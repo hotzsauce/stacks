f627e6928bf095292b77b9499116116867f04231
Logan Hotz <logan@modo.energy>
2025-05-06 21:16:57 -0400

    init python bindings

---
+++ stacks/src/lib.rs
@@ -0,0 +1,33 @@
+use pyo3::exceptions::PyRuntimeError;
+use pyo3::prelude::*;
+use rusty_stacks::IncStack;
+
+#[pyclass]
+struct OfferStack {
+    inner: IncStack<f64, f64>,
+}
+
+#[pymethods]
+impl OfferStack {
+    #[new]
+    fn new(x: Vec<f64>, y: Vec<f64>) -> PyResult<Self> {
+        IncStack::try_from_vectors(x, y)
+            .map(|inner| Self { inner })
+            .map_err(|_| PyRuntimeError::new_err("encountered error"))
+    }
+
+    fn cumulate(&self) -> Vec<f64> {
+        self.inner.cumulate()
+    }
+
+    fn __repr__(&self) -> String {
+        // impractical for large Stacks but sufficient for prototyping lol
+        format!("Stack({:?}, {:?})", self.inner.x, self.inner.y)
+    }
+}
+
+#[pymodule]
+fn stacks(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<OfferStack>()?;
+    Ok(())
+}
